6d46d878a2fcf2c72a351371ea1dd70333af2f6e
sort-nlogn <artomstepanov443@gmail.com>
2021-03-29 14:48:31 +0300

    Player

---
+++ src/components/Player.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>{{pos}}</h3>
+        <h3>{{name}}</h3>
+        <button v-on:click = "show()">Show</button>
+    </div>
+    <div v-if = "show">
+        {{player}}
+    </div>
+</template>
+
+<script>
+
+export default defineComponent({
+    name: "Player",
+    props: {
+        pos: Number,
+        name: String
+    },
+    data() {
+        show = false
+    },
+    methods: {
+        show(){
+            this.show = !this.show
+        }
+    }
+})
+</script>
